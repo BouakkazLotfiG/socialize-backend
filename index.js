66cfc0462f23bc1a06471be289f49c788ee6270e
Lotfi <bouakkaz144.lotfi@gmail.com>
2022-12-25 20:53:18 +0100

    cors added

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ import { verifyToken } from './middleware/auth.js';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
-import cors from 'cors';
 
 // import User from './models/User.js';
 // import Post from './models/Post.js';
@@ -31,7 +30,6 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-app.use(cors());
 
 // file storage
 const storage = multer.diskStorage({
