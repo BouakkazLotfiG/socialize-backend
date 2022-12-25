e6d5639e8b211e51c039a5a55d8fae06bbeb6864
Lotfi <bouakkaz144.lotfi@gmail.com>
2022-12-25 21:03:33 +0100

    cors added

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // file storage
 const storage = multer.diskStorage({
