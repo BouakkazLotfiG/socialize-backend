c268fd32db3ee2f95b6fb41d6c46f0ae1b466371
Lotfi <bouakkaz144.lotfi@gmail.com>
2022-12-25 20:48:00 +0100

    cors added

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import { verifyToken } from './middleware/auth.js';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
+import cors from 'cors';
 
 // import User from './models/User.js';
 // import Post from './models/Post.js';
@@ -30,6 +31,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(cors());
 
 // file storage
 const storage = multer.diskStorage({
@@ -54,10 +56,13 @@ app.use('/posts', postRoutes);
 // mongoose set
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect('mongodb+srv://lotfi:lotfi@cluster0.3qtafqe.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    'mongodb+srv://lotfi:lotfi@cluster0.3qtafqe.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
 
