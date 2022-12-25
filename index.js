e5e05c02862b4693fb39f6c77523a17df59f906f
Lotfi <bouakkaz144.lotfi@gmail.com>
2022-12-25 21:13:23 +0100

    cors added

--- index.js
+++ index.js
@@ -28,9 +28,8 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-app.use(function (req, res, next) {
 
 // file storage
 const storage = multer.diskStorage({
